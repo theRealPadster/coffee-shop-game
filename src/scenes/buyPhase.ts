d33640518c5bd3c981b230c3f1d3a9a4cd260cbf
Isaac <padspambox@gmail.com>
2026-05-30 17:35:03 -0400

    disable buy buttons you can't afford

--- src/scenes/buyPhase.ts
+++ src/scenes/buyPhase.ts
@@ -157,9 +157,9 @@ function shopRow(state: GameState, ing: Ingredient, r: GameState['recipes']['hot
         <div class="stock"><strong>${stock}</strong> <span class="stock-unit">in stock</span></div>
         <div class="price"><strong>${formatCents(price)}</strong> <span class="price-unit">each</span> ${priceChip(level)}${priceSparkline(state.priceHistory[ing], PRICE_BANDS[ing])}</div>
         <div class="controls">
-          <button class="buy-btn" data-buy="${ing}" data-qty="5">Buy 5</button>
-          <button class="buy-btn" data-buy="${ing}" data-qty="10">Buy 10</button>
-          <button class="buy-btn" data-buy="${ing}" data-qty="20">Buy 20</button>
+          <button class="buy-btn" data-buy="${ing}" data-qty="5" ${state.cash < price * 5 ? 'disabled' : ''}>Buy 5</button>
+          <button class="buy-btn" data-buy="${ing}" data-qty="10" ${state.cash < price * 10 ? 'disabled' : ''}>Buy 10</button>
+          <button class="buy-btn" data-buy="${ing}" data-qty="20" ${state.cash < price * 20 ? 'disabled' : ''}>Buy 20</button>
           <span class="buy-cost">${formatCents(price * 5)}</span>
           <span class="buy-cost">${formatCents(price * 10)}</span>
           <span class="buy-cost">${formatCents(price * 20)}</span>
